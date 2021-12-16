812b89c99ae51a16339828574ccca18a0c01a51e
slappygig <96138262+slappygig@users.noreply.github.com>
2021-12-16 13:30:04 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# prodigy-classic
+DO NOT DMCA THIS, ITS A FAN MADE GAME OF PRODIGY MATH GAME! and the scripts for no math and member are: javascript: Phaser.GAMES[0.].prodigy.debug.easyMode(1,1) javascript: Phaser.GAMES[0.].prodigy.player.isMember = true
---
+++ game.js
@@ -0,0 +1,44 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(function(){
+	var sw;
+	sw = window.sw || {};
+	sw.overlayManager = sw.overlayManager || new SW.OverlayManagerView({el: "#overlay-mask"});
+	sw.toastManager = new SW.ToastManager();
+	sw.dialogManager = sw.dialogManager || new SW.DialogManager();
+	sw.track = sw.track || new SW.Track();
+	sw.config = sw.config || new SW.Config();
+})();
+
+}
+/*
+     FILE ARCHIVED ON 23:01:41 Jan 06, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:23:01 Jun 22, 2020.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots.policy: 0.199
+  esindex: 0.014
+  RedisCDXSource: 34.751
+  captures_list: 502.832
+  load_resource: 768.276
+  LoadShardBlock: 451.311 (3)
+  PetaboxLoader3.datanode: 600.756 (5)
+  CDXLines.iter: 12.92 (3)
+  exclusion.robots: 0.215
+  PetaboxLoader3.resolve: 557.245 (3)
+*/
